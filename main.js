52108b34b1aa74638bc1c20c0868305ec5221103
diwacreation3 <diwakarcreation@gmail.com>
2024-03-27 22:02:59 +0545

    First commit 🍃

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+
+//image slider 
+function imageSlider(anything) 
+{
+    document.querySelector('.pepsi').src = anything;
+
+}
+
+function changeBgColor(color){
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+}
